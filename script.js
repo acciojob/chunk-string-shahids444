faf20980324c14e784ffc7cc7fd43ae3c2797393
shahids444 <shahidshaiks444@gmail.com>
2025-05-02 21:20:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-function stringChop(str, size) {
-  if (size <= 0) return []; // Edge case: invalid size
+function stringChop(str, chunkLength) {
+  if (str === null || chunkLength <= 0) return [];
 
   let result = [];
-  for (let i = 0; i < str.length; i += size) {
-    result.push(str.substr(i, size));
+  for (let i = 0; i < str.length; i += chunkLength) {
+    result.push(str.substring(i, i + chunkLength));
   }
   return result;
 }
+
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = parseInt(prompt("Enter Chunk Size."));
